f5629878c0ca1de3ad4c80750cc1a891d9880556
Ehtesham <ehtesham_1234@live.com>
2024-01-22 01:57:43 +0530

    Fixes wrong import

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,5 +1,5 @@
 import { createBooking, updateHotelRoom } from "@/lib/api";
-import { CreateBookingDTO } from "@/models/room";
+import { CreateBookingDTO } from "@/models/booking";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
